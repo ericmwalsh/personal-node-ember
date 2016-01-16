677863d599a3ae04c3f55fdeb2eb2a0cf0bb43be
Eric Walsh <walsh@spinlister.com>
2016-01-16 15:36:37 -0800

    Added font awesome, fixed emblem, started header and footer.  Added SCSS support.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,9 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
+    emberCliFontAwesome: {
+      useScss: true
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
@@ -19,6 +22,11 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+  app.import(app.bowerDirectory + '/tether/dist/js/tether.js');
+  app.import(app.bowerDirectory + '/tether/dist/css/tether.css');
+
+  app.import(app.bowerDirectory + '/bootstrap/dist/js/bootstrap.js');
+  app.import(app.bowerDirectory + '/bootstrap/dist/css/bootstrap.css');
 
   return app.toTree();
 };
