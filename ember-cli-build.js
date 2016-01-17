6589461c7603c8c4ff89d8d5a719ce18beb937ba
Eric Walsh <walsh@spinlister.com>
2016-01-16 19:11:14 -0800

    added bootstrap v3.3.6

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,10 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
+    emberCliFontAwesome: {
+      // option not working
+      // useScss: true
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
@@ -19,6 +23,9 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+
+  app.import(app.bowerDirectory + '/bootstrap/dist/js/bootstrap.js');
+  app.import(app.bowerDirectory + '/bootstrap/dist/css/bootstrap.css');
 
   return app.toTree();
 };
